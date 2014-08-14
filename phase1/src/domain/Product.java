da25dc9c5e37988bb125470a599e6697d14f7169
Kendall Lauren Chin <Kendall Lauren Chin@192.168.1.105>
2014-08-15 01:49:37 +1200

    Added getters and setters for the fields, and a toString() to display the ID and Name.

--- phase1/src/domain/Product.java
+++ phase1/src/domain/Product.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package domain;
 
 /**
@@ -11,11 +10,77 @@ package domain;
  * @author Kendall Lauren Chin
  */
 public class Product {
-    private Integer Id;
+
+    private Integer id;
     private String name;
     private String description;
     private String category;
     private Double price;
     private Integer Quantity;
-    
+
+    public Product(Integer id, String name, String description, String category, Double price, Integer Quantity) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.category = category;
+        this.price = price;
+        this.Quantity = Quantity;
+    }
+
+    public Product() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer Id) {
+        this.id = Id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getQuantity() {
+        return Quantity;
+    }
+
+    public void setQuantity(Integer Quantity) {
+        this.Quantity = Quantity;
+    }
+
+    @Override
+    public String toString() {
+        return id + ", " + name;
+    }
+
 }
