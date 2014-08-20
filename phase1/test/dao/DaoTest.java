2d3722a63a7b2c093506e6964d4c6c8bca148a6c
Kendall Lauren Chin <Kendall Lauren Chin@192.168.1.105>
2014-08-20 19:31:41 +1200

    Created a test method for the getById, getByCategory, getCategories, save and edit methods.

--- phase1/test/dao/DaoTest.java
+++ phase1/test/dao/DaoTest.java
@@ -1,12 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package dao;
 
 import domain.Product;
+import java.util.Collection;
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertNull;
 import org.junit.After;
@@ -21,33 +16,34 @@ import org.junit.Test;
  * @author Kendall Lauren Chin
  */
 public class DaoTest {
+
     private ProductDAO dao = new ProductCollectionDAO();
-    
+
     private int ballId = 1234;
     private String ballDescription = "Round";
     private String ballCategory = "Toy";
     private Double ballPrice = 5.20;
     private int ballQuantity = 1000;
     private Product ball;
-    
+
     private int dollId = 4567;
     private String dollDescription = "Pretty";
     private String dollCategory = "Antique";
     private Double dollPrice = 35.80;
     private int dollQuantity = 2000;
     private Product doll;
-    
+
     public DaoTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
         ball = new Product(ballId, "Ball", ballDescription, ballCategory, ballPrice, ballQuantity);
@@ -55,7 +51,7 @@ public class DaoTest {
         dao.save(doll);
         dao.save(ball);
     }
-    
+
     @After
     public void tearDown() {
         dao.delete(doll);
@@ -64,20 +60,93 @@ public class DaoTest {
 
     @Test
     public void testDaoSaveAndDelete() {
-// create student for testing
         Product Lipstick = new Product(7891, "Lipstick", "Red, matte", "Makeup", 40.50, 6054);
-// save the student using DAO
         dao.save(Lipstick);
-// retrieve the same student via DAO
         Product retrieved = dao.getById(7891);
-// ensure that the student we saved is the one we got back
         assertEquals("Retrieved student should be the same as the saved one",
                 Lipstick, retrieved);
-// delete the student via the DAO
         dao.delete(Lipstick);
-// try to retrieve the deleted student
         retrieved = dao.getById(7891);
-// ensure that the student was not retrieved (should be null)
         assertNull("Product should no longer exist", retrieved);
+    }
+
+    @Test
+    public void testDaoGetAll() {
+        Collection<Product> products = dao.getAll();
+        assertTrue("Doll should exist in the result", products.contains(doll));
+        assertTrue("Ball should exist in the result", products.contains(ball));
+        for (Product s : products) {
+            if (s.equals(doll)) {
+                assertEquals(doll.getId(), s.getId());
+                assertEquals(doll.getName(), s.getName());
+                assertEquals(doll.getCategory(), s.getCategory());
+                assertEquals(doll.getDescription(), s.getDescription());
+                assertEquals(doll.getPrice(), s.getPrice());
+                assertEquals(doll.getQuantity(), s.getQuantity());
+            }
+        }
+    }
+
+    @Test
+    public void testDaoGetById() {
+        Product retrieved = dao.getById(dollId);
+        assertEquals("Retrieved student should be the same as the saved one",
+                doll, retrieved);
+        Collection<Product> products = dao.getAll();
+        for (Product s : products) {
+            if (s.equals(doll)) {
+                assertEquals(doll.getId(), s.getId());
+                assertEquals(doll.getName(), s.getName());
+                assertEquals(doll.getCategory(), s.getCategory());
+                assertEquals(doll.getDescription(), s.getDescription());
+                assertEquals(doll.getPrice(), s.getPrice());
+                assertEquals(doll.getQuantity(), s.getQuantity());
+            }
+        }
+        Integer fakeId = 999999999;
+        assertEquals("Making sure a non existant ID returns a null", dao.getById(fakeId), null);
+    }
+
+    @Test
+    public void testDaoGetCategories() {
+        Collection<String> retrieved = dao.getCategories();
+        assertTrue("Ball category should exist in the result", retrieved.contains(ball.getCategory()));
+        assertTrue("Doll category should exist in the result", retrieved.contains(doll.getCategory()));
+    }
+
+    @Test
+    public void testDaoGetByCategories() {
+        Collection<Product> retrieved = dao.getByCategories(ballCategory);
+        assertTrue("Ball should exist in the result", retrieved.contains(ball));
+        assertEquals("Doll should not exist in the result", retrieved.contains(doll), false);
+
+        Collection<Product> products = dao.getAll();
+        for (Product s : products) {
+            if (s.equals(doll)) {
+                assertEquals(doll.getId(), s.getId());
+                assertEquals(doll.getName(), s.getName());
+                assertEquals(doll.getCategory(), s.getCategory());
+                assertEquals(doll.getDescription(), s.getDescription());
+                assertEquals(doll.getPrice(), s.getPrice());
+                assertEquals(doll.getQuantity(), s.getQuantity());
+            }
+        }
+        String fakeCategory = "Clothing";
+        assertNull("Making sure a non existant category returns a null", dao.getByCategories(fakeCategory));
+    }
+
+    @Test
+    public void testDaoSaveEdit() {
+        Product p = dao.getById(ballId);
+        String newName = "Bell";
+        p.setName(newName);
+        dao.save(p);
+        Product s = dao.getById(ballId);
+        assertEquals(p.getId(), s.getId());
+        assertEquals(p.getName(), s.getName());
+        assertEquals(p.getCategory(), s.getCategory());
+        assertEquals(p.getDescription(), s.getDescription());
+        assertEquals(p.getPrice(), s.getPrice());
+        assertEquals(p.getQuantity(), s.getQuantity());
     }
 }
