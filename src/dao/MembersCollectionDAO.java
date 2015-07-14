1d315e37056f3c8e366434c5e0b752762b6e15d4
brase127 <brase127@mds-xa101s-p068.student.otago.ac.nz>
2015-07-14 14:57:12 +1200

    Implemented interface methods, made a treemap.

--- src/dao/MembersCollectionDAO.java
+++ src/dao/MembersCollectionDAO.java
@@ -5,10 +5,36 @@
  */
 package dao;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
 /**
  *
  * @author brase127
  */
-public class MembersCollectionDAO {
+public class MembersCollectionDAO implements MemberDAO{
     
+    Map<String, Member> memberCollection = new TreeMap<>();
+
+
+    public void add(String id, Member member) {
+        memberCollection.put(id, member); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void delete(String id) {
+        memberCollection.remove(id); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void edit(String id, Member member) {
+        memberCollection.replace(id, member); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Map getMembers() {
+        return memberCollection; //To change body of generated methods, choose Tools | Templates.
+    }
+
 }
